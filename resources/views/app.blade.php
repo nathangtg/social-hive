24fe71238dd2117a4fc71184b1396a5f89efcf7b
nathangtg <22099865@imail.sunway.edu.my>
2024-02-19 11:23:48 +0800

    Added Icon for the webpage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
+        <link rel="icon" href="{{ asset('css/favicon.ico') }}">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
